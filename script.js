90f56374613adccf6d80b64986e30f9a7622378d
Daniel Dekany <104760231+dknyd@users.noreply.github.com>
2023-06-24 11:11:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttonLeft = document.querySelector(".buttonLeft");
 const buttonRight = document.querySelector(".buttonRight");
 const tile1 = document.querySelector(".tile1");
 
+tile1.style.backgroundImage = "images/desktop-image-hero-1.jpg";
+
 const tile2BigText = document.querySelector("h1");
 const tile2SmallText = document.querySelector(".text1");
 
@@ -69,53 +71,3 @@ if (window.innerWidth < 1300) {
 
 updateSlideContent();
 
-
-
-
-// const buttonLeft = document.querySelector(".buttonLeft");
-// const buttonRight = document.querySelector(".buttonRight");
-// const tile1 = document.querySelector(".tile1");
-
-// const tile2BigText = document.querySelector("h1");
-// const tile2SmallText = document.querySelector(".text1");
-
-
-// let currentSlideMobile = 0;
-// const slide1Pic = "images/mobile-image-hero-1.jpg";
-// const slide2Pic = "images/mobile-image-hero-2.jpg";
-// const slide3Pic = "images/mobile-image-hero-3.jpg";
-// const picArrayMobile = [slide1Pic, slide2Pic, slide3Pic];
-
-
-// let currentBigText = 0;
-// const slide1BigText = "Discover innovative ways to decorate";
-// const slide2BigText = "We are available all across the globe";
-// const slide3BigText = "Manufactured with the best materials";
-// const bigTextArray = [slide1BigText, slide2BigText, slide3BigText];
-
-// let currentSmallText = 0;
-// const slide1SmallText = "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.";
-// const slide2SmallText = "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.";
-// const slide3SmallText = "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.";
-// const smallTextArray = [slide1SmallText, slide2SmallText, slide3SmallText];
-
-
-// buttonRight.addEventListener("click", function(){
-// 	if (currentSlideMobile < 2){
-// 		currentSlideMobile ++;
-// 		currentBigText ++;
-// 		currentSmallText ++;
-// 		tile1.style.backgroundImage = `url("${picArrayMobile[currentSlideMobile]}")`;
-// 		tile2BigText.innerHTML = bigTextArray[currentBigText];
-// 		tile2SmallText.innerHTML = smallTextArray[currentSmallText];
-// 	} })
-
-// buttonLeft.addEventListener("click", function(){
-// 	if (currentSlideMobile > 0){
-// 		currentSlideMobile --;
-// 		currentBigText --;
-// 		currentSmallText --;
-// 		tile1.style.backgroundImage = `url("${picArrayMobile[currentSlideMobile]}")`;
-// 		tile2BigText.innerHTML = bigTextArray[currentBigText];
-// 		tile2SmallText.innerHTML = smallTextArray[currentSmallText];
-// 	} })
